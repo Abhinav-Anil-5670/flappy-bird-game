e5dcbef198ff119fbe61b7cf0b572acf4b981fb2
Abhinav-Anil-5670 <abhinavanil800@gmail.com>
2025-08-22 21:05:48 +0530

    Added_the_pipes_into_the_canvas

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ let birdX = boardWidth/8
 let birdY = boardHeight/2
 let birdImage
 
+//pipes
+let pipeArray = []
+let pipeWidth = 64
+let pipeHeight = 512
+let pipeX = boardWidth
+let pipeY = 0 
+
+let topPipeImg
+let BottomPipeImg
+
+
 let bird = {
     x: birdX,  y : birdY , width : birdWidth , height : birdHeight
 }
@@ -30,5 +41,41 @@ window.onload = ()=>{
     birdImage.onload = ()=>{
         context.drawImage(birdImage,bird.x, bird.y , bird.width, bird.height)
     }
+    requestAnimationFrame(update)
 
+    topPipeImg = new Image()
+    topPipeImg.src = "./images/toppipe.png"
+
+    BottomPipeImg = new Image()
+    BottomPipeImg.src = './images/bottompipe.png'
+    setInterval(placePipes,1500)
+
+}
+
+function update(){
+
+    requestAnimationFrame(update)
+    context.clearRect(0,0,boardWidth,birdHeight)
+
+    context.drawImage(birdImage,bird.x, bird.y , bird.width, bird.height)
+
+    //pipes
+    for(let i = 0; i < pipeArray.length;i++){
+        let pipe = topPipe[i]
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height)
+    }
+
+}
+
+function placePipes(){
+    let topPipe = {
+        img : topPipeImg,
+        x : pipeX,
+        y : pipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(topPipe)
 }
